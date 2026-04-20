29a6e7b4d616cd535317205ada19038f8226d9e3
Faisal Khan <faisal.cn42@gmail.com>
2026-04-20 13:16:46 +1000

    finalized spellcast to a deployable state

--- src/games/spellcast/constants/gameConfig.js
+++ src/games/spellcast/constants/gameConfig.js
@@ -3,7 +3,7 @@ export const MAX_PLAYERS = 6
 
 export const BOARD_SIZE = 5
 export const BOARD_WORD_LENGTHS = [5, 4, 4, 4, 4, 4]
-export const SHOW_RUNE_FIELD_BOX = true
+export const SHOW_RUNE_FIELD_BOX = false
 export const MIN_NUM_ROUNDS = 5
 export const MAX_NUM_ROUNDS = 10
 export const DEFAULT_NUM_ROUNDS = 5
